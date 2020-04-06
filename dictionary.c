3f9fe59d3a741e240dc17735b5c7821a589d2aa9
Alex <a.withsmith@gmail.com>
2020-04-06 11:11:22 +0100

    Resetting some minor change attempts

--- dictionary.c
+++ dictionary.c
@@ -39,7 +39,6 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // Implementation of djb2
-    // http://www.cse.yorku.ca/~oz/hash.html
     unsigned int hashAddress = 5381;
     for (int counter = 0; word[counter]!='\0'; counter++)
     {
